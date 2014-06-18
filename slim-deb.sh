9a432b50d195c8a7780f6166a9bedecf4598b9ba
Danijel J <danijel.james@me.com>
2014-06-18 20:25:17 +1000

    Create slim-deb.sh
    Slims down Debian

---
+++ slim-deb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Slims down Debian x86_64
+#
+sudo apt-get -y --purge remove acpi acpid aptitude at aspell aspell-en avahi-daemon base-config bc bin86 bind9-host ca-certificates console-common console-data console-tools cupsys-client cupsys-bsd cupsys-driver-gutenprint cupsys-pdf dc debian-faq debian-faq-de debian-faq-fr debian-faq-it debian-faq-zh-cn dhcp DHCP Client dhcp3-client DHCP Client dhcp3-common DHCP Client dictionaries dnsutils doc-debian doc-linux-text eject fdutils finger foomatic-filters gcc-3.3 gettext-base groff gnupg hplip iamerican ibritish info ispell laptop-detect libavahi-compat-libdnssd1 libc6-i686 Not recommended for generic 486 machines using generic IA32 architecture libgpmg1 manpages mtools mtr-tiny mutt netcat net-tools ncurses-term pppconfig read-edid reportbug smclient tasksel tcsh traceroute vim-common  vim-tiny  wamerican w3m whois zeroinstall-injector
