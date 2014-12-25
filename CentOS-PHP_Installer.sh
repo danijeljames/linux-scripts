1d3862db6af9d71f9ef1586f7b56cbdc5d81fd29
Danijel J <danijel.james@me.com>
2014-12-26 01:02:34 +1100

    Update and rename centosphp.sh to CentOS-PHP_Installer.sh

---
+++ CentOS-PHP_Installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#############
+## Global Parameters
+#############
+RELEASEVER=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
+todaysDateUS="$(date +'%Y%m%d')"
+
+#############
+## Terminal Colours
+#############
+echo "export CLICOLOR=1" >> ~/.bash_profile
+echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ~/.bash_profile
+echo "export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
+source ~/.bash_profile
+
+#############
+## Update Server First
+#############
+yum -y update
+
+#############
+## Install PHP 5.6 on CentOS 5/6/7 x86 or x64
+#############
+if [ "$RELEASEVER" = "7" ]; then
+  rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
+  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+else
+  if [ "$RELEASEVER" = "6" ]; then
+    rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+  else
+    if  [ "$RELEASEVER" = "5" ]; then
+      rpm -Uvh https://mirror.webtatic.com/yum/el5/latest.rpm
+    fi
+  fi
+fi
+yum -y install php56w php56w-opcache
+yum -y install php56w-opcache
--- centosphp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "export CLICOLOR=1" >> ~/.bash_profile
-echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ~/.bash_profile
-echo "export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bash_profile
-source ~/.bash_profile
-yum -y update
-rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
-yum -y install php56w php56w-opcache
-yum -y install php56w-opcache
